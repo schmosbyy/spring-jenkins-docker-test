34a27559f65487780fc7475fd834ad9265b64f92
Sakaar Poply <spoply5910@conestogac.on.ca>
2024-06-07 20:26:38 -0400

    first commit

---
+++ src/main/java/com/example/spring_jenkins_docker_test/SpringJenkinsDockerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jenkins_docker_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsDockerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsDockerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_jenkins_docker_test/SpringJenkinsDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jenkins_docker_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

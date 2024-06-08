7e578930245bd8fbc69d1edf8a1c9d0db4ac5946
Sakaar Poply <spoply5910@conestogac.on.ca>
2024-06-07 20:44:41 -0400

    second commit

--- src/main/java/com/example/spring_jenkins_docker_test/SpringJenkinsDockerTestApplication.java
+++ src/main/java/com/example/spring_jenkins_docker_test/SpringJenkinsDockerTestApplication.java
@@ -8,6 +8,7 @@ public class SpringJenkinsDockerTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsDockerTestApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
